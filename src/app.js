4dd545680ad210732260cb98567f5617e392a5b0
Erick Damian <erick.damian@curantissolutions.com>
2025-05-21 19:27:17 -0600

    fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://your-frontend-domain.com' 
+    ? ['http://35.87.197.249', 'http://ec2-35-87-197-249.us-west-2.compute.amazonaws.com']
     : 'http://localhost:3000',
   credentials: true
 }));
